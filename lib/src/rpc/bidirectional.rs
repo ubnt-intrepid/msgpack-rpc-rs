0d508f6f2980a9be19b64ed7a53701f0bd1a8c55
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-07-31 19:00:18 +0900

    bidirectional transport

--- lib/src/rpc/bidirectional.rs
+++ lib/src/rpc/bidirectional.rs
@@ -1,57 +1,41 @@
 use std::error;
 use std::io;
 use std::marker::PhantomData;
-use futures::{Stream, Sink, Poll, Async, AsyncSink, StartSend};
+use futures::{Stream, Sink, Poll, AsyncSink, StartSend};
 use futures::sync::mpsc;
+use tokio_core::reactor::Handle;
 use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::codec::{FramedRead, FramedWrite};
 
-use super::codec::Transport;
+use super::codec::Codec;
 use super::message::{Message, Request, Response};
 
-pub struct ClientTransport<T: AsyncRead + AsyncWrite + 'static> {
-    inner: Transport<T>,
-    tx_req: mpsc::Sender<(u64, Request)>,
-    _rx_res: mpsc::Receiver<(u64, Response)>,
+
+pub struct ClientTransport<T> {
+    rx_res: mpsc::Receiver<(u64, Response)>,
+    tx_select: mpsc::Sender<Message>,
+    _marker: PhantomData<T>,
 }
 
-impl<T: AsyncRead + AsyncWrite + 'static> ClientTransport<T> {
-    fn send_request(&mut self, mut msg: (u64, Request)) -> io::Result<()> {
-        loop {
-            msg = match self.tx_req.start_send(msg) {
-                Ok(AsyncSink::Ready) => break Ok(()),
-                Ok(AsyncSink::NotReady(msg)) => msg,
-                Err(err) => break Err(io::Error::new(io::ErrorKind::Other, format!("{:?}", err))),
-            }
-        }
-    }
-}
-
-impl<T: AsyncRead + AsyncWrite + 'static> Stream for ClientTransport<T> {
+impl<T> Stream for ClientTransport<T> {
     type Item = (u64, Response);
     type Error = io::Error;
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        loop {
-            match try_ready!(self.inner.poll()) {
-                Some(Message::Response(id, res)) => break Ok(Async::Ready(Some((id, res)))),
-                Some(Message::Request(id, req)) => {
-                    self.send_request((id, req))?;
-                    continue;
-                }
-                Some(_) => unreachable!(),
-                None => break Ok(Async::Ready(None)),
-            }
-        }
+        self.rx_res.poll().map_err(|()| {
+            io::Error::new(io::ErrorKind::Other, "rx")
+        })
     }
 }
 
-impl<T: AsyncRead + AsyncWrite + 'static> Sink for ClientTransport<T> {
+impl<T> Sink for ClientTransport<T> {
     type SinkItem = (u64, Request);
     type SinkError = io::Error;
 
     fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
-        // TODO: send response if rx is not empty
-        match self.inner.start_send(Message::Request(item.0, item.1))? {
+        match self.tx_select
+            .start_send(Message::Request(item.0, item.1))
+            .map_err(into_io_error)? {
             AsyncSink::Ready => Ok(AsyncSink::Ready),
             AsyncSink::NotReady(Message::Request(id, req)) => Ok(AsyncSink::NotReady((id, req))),
             AsyncSink::NotReady(_) => unreachable!(),
@@ -59,18 +43,19 @@ impl<T: AsyncRead + AsyncWrite + 'static> Sink for ClientTransport<T> {
     }
 
     fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
-        self.inner.poll_complete()
+        self.tx_select.poll_complete().map_err(into_io_error)
     }
 }
 
 
-pub struct ServerTransport<T: AsyncRead + AsyncWrite + 'static> {
-    tx_res: mpsc::Sender<(u64, Response)>,
+
+pub struct ServerTransport<T> {
     rx_req: mpsc::Receiver<(u64, Request)>,
+    tx_select: mpsc::Sender<Message>,
     _marker: PhantomData<T>,
 }
 
-impl<T: AsyncRead + AsyncWrite + 'static> Stream for ServerTransport<T> {
+impl<T> Stream for ServerTransport<T> {
     type Item = (u64, Request);
     type Error = io::Error;
 
@@ -81,41 +66,104 @@ impl<T: AsyncRead + AsyncWrite + 'static> Stream for ServerTransport<T> {
     }
 }
 
-impl<T: AsyncRead + AsyncWrite + 'static> Sink for ServerTransport<T> {
+impl<T> Sink for ServerTransport<T> {
     type SinkItem = (u64, Response);
     type SinkError = io::Error;
 
     fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
-        self.tx_res.start_send(item).map_err(into_io_error)
+        match self.tx_select
+            .start_send(Message::Response(item.0, item.1))
+            .map_err(into_io_error)? {
+            AsyncSink::Ready => Ok(AsyncSink::Ready),
+            AsyncSink::NotReady(Message::Response(id, res)) => Ok(AsyncSink::NotReady((id, res))),
+            AsyncSink::NotReady(_) => unreachable!(),
+        }
     }
 
     fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
-        self.tx_res.poll_complete().map_err(into_io_error)
+        self.tx_select.poll_complete().map_err(into_io_error)
     }
 }
 
 
-fn into_io_error<E: error::Error>(err: E) -> io::Error {
-    io::Error::new(io::ErrorKind::Other, format!("{:?}", err))
+/// Create a pair of client-side / server-side transports from given asynchronous I/O.
+///
+/// Note that this function will spawn some background task.
+pub fn make_transports<T>(io: T, handle: &Handle) -> (ClientTransport<T>, ServerTransport<T>)
+where
+    T: AsyncRead + AsyncWrite + 'static,
+{
+    // TODO: set buffer size
+    let (mut tx_req, rx_req) = mpsc::channel(1);
+    let (mut tx_res, rx_res) = mpsc::channel(1);
+    let (tx_select, rx_select) = mpsc::channel(1);
+
+    let client = ClientTransport {
+        rx_res,
+        tx_select: tx_select.clone(),
+        _marker: PhantomData,
+    };
+
+    let server = ServerTransport {
+        rx_req,
+        tx_select: tx_select.clone(),
+        _marker: PhantomData,
+    };
+
+    let (read, write) = io.split();
+    let stream = FramedRead::new(read, Codec);
+    let mut sink = FramedWrite::new(write, Codec);
+
+    // A background task to receive raw messages.
+    // It will send received messages to client/server transports.
+    // TODO: treat notification message
+    handle.spawn(stream.map_err(|_| ()).for_each(
+        move |msg| -> Result<(), ()> {
+            match msg {
+                Message::Request(id, req) => {
+                    start_send_until_ready(&mut tx_req, (id, req)).map_err(
+                        |_| (),
+                    )?;
+                }
+                Message::Response(id, res) => {
+                    start_send_until_ready(&mut tx_res, (id, res)).map_err(
+                        |_| (),
+                    )?;
+                }
+                Message::Notification(_not) => (),
+            }
+            Ok(())
+        },
+    ));
+
+    // A background task to send messages.
+    handle.spawn(rx_select.map_err(|_| ()).for_each(move |msg| {
+        fn loop_fn<S: Sink<SinkItem = Message>>(sink: &mut S, msg: Message) -> Result<(), ()> {
+            match sink.start_send(msg) {
+                Ok(AsyncSink::Ready) => Ok(()),
+                Ok(AsyncSink::NotReady(msg)) => loop_fn(sink, msg),
+                Err(_) => return Err(()),
+            }
+        }
+        loop_fn(&mut sink, msg)
+    }));
+
+    (client, server)
 }
 
 
-pub fn transports<T: AsyncRead + AsyncWrite + 'static>(
-    io: T,
-) -> (ClientTransport<T>, ServerTransport<T>) {
-    let inner = Transport::from(io);
-    // TODO: set buffer size
-    let (tx_req, rx_req) = mpsc::channel(1);
-    let (tx_res, _rx_res) = mpsc::channel(1);
-    let client = ClientTransport {
-        inner,
-        tx_req,
-        _rx_res,
-    };
-    let server = ServerTransport {
-        rx_req,
-        tx_res,
-        _marker: PhantomData,
-    };
-    (client, server)
+fn start_send_until_ready<S: Sink>(
+    sink: &mut S,
+    mut item: S::SinkItem,
+) -> Result<(), S::SinkError> {
+    loop {
+        item = match sink.start_send(item)? {
+            AsyncSink::Ready => break Ok(()),
+            AsyncSink::NotReady(item) => item,
+        }
+    }
+}
+
+fn into_io_error<E: error::Error>(err: E) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, format!("{:?}", err))
 }
