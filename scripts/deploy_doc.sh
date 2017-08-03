750f05ac21c3223c3add290ff868e10058cd95fb
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-08-03 22:32:18 +0900

    disable cobalt

--- scripts/deploy_doc.sh
+++ scripts/deploy_doc.sh
@@ -10,8 +10,8 @@ TRAVIS_REPO_SLUG=$4
 if [[ "${TRAVIS_BRANCH:-}" = "master" ]] && [[ "${TRAVIS_RUST_VERSION}" = "stable" ]]; then
     cargo doc -p msgpack-rpc --all-features --no-deps
 
-    cargo install --force cobalt-bin
-    cobalt build -s site -d target/doc
+    # cargo install --force cobalt-bin
+    # cobalt build -s site -d target/doc
 
     ghp-import -n target/doc
     git push -qf "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages
