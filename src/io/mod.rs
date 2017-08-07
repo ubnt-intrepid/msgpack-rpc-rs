cd9b4e9b492173a0f6a7e8e701633b195d7b7a01
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-08-07 15:21:50 +0900

    add helper functions

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,5 +1,5 @@
 //!
-//! I/O streams
+//! definition of I/O streams and helper functions.
 //!
 
 mod stdio;
@@ -7,3 +7,41 @@ mod process;
 
 pub use self::stdio::StdioStream;
 pub use self::process::ChildProcessStream;
+
+use std::sync::Arc;
+use futures::Stream;
+use futures::future::empty;
+use tokio_core::net::TcpListener;
+use tokio_core::reactor::Core;
+use super::{Endpoint, Handler};
+
+/// Run the RPC server on standard input/standard output, with given handler.
+pub fn run_stdio<H: Handler>(handler: H, chunk_size: usize) {
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+    let io = StdioStream::new(chunk_size);
+    let endpoint = Endpoint::from_io(&handle, io);
+
+    endpoint.serve(&handle, handler);
+    core.run(empty::<(), ()>()).unwrap();
+}
+
+/// Run the RPC server on TCP, with given handler.
+pub fn run_tcp<H: Handler>(handler: H, addr: &str) {
+    let handler = Arc::new(handler);
+
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+    let addr = addr.parse().unwrap();
+    let listener = TcpListener::bind(&addr, &handle).unwrap();
+
+    let server = listener.incoming().for_each(move |(sock, _addr)| {
+        let endpoint = Endpoint::from_io(&handle, sock);
+        endpoint.serve(&handle, handler.clone());
+        Ok(())
+    });
+
+    core.run(server).unwrap();
+}
