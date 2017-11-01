b2c953bc9d3c1012f800cc0d10b329def0b32a5c
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-11-01 16:11:41 +0900

    fix(*): remove deprecated 'boxed()'

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -10,6 +10,7 @@ use tokio_core::net::TcpStream;
 use tokio_core::reactor::Core;
 use msgpack_rpc::{Endpoint, Value};
 use structopt::StructOpt;
+use std::io;
 use std::net::SocketAddr;
 
 #[derive(StructOpt)]
@@ -33,23 +34,22 @@ fn main() {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
 
-    let task = TcpStream::connect(&opt.addr, &handle).and_then(move |stream| {
-        let method = opt.method.as_str();
-        let args = Value::Array(opt.args.into_iter().map(Into::into).collect::<Vec<Value>>());
+    let task = TcpStream::connect(&opt.addr, &handle).and_then(
+        move |stream| -> Box<Future<Item = (), Error = io::Error>> {
+            let method = opt.method.as_str();
+            let args = Value::Array(opt.args.into_iter().map(Into::into).collect::<Vec<Value>>());
 
-        let client = Endpoint::from_io(&handle, stream).into_client();
-        if opt.notify {
-            client.notify(method, args).boxed()
-        } else {
-            client
-                .request(method, args)
-                .and_then(|response| {
+            let client = Endpoint::from_io(&handle, stream).into_client();
+            if opt.notify {
+                Box::new(client.notify(method, args))
+            } else {
+                Box::new(client.request(method, args).and_then(|response| {
                     println!("{:?}", response);
                     Ok(())
-                })
-                .boxed()
-        }
-    });
+                }))
+            }
+        },
+    );
 
     if let Err(e) = core.run(task) {
         eprintln!("failed with: {}", e);
